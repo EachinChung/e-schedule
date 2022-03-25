a285f40b3afc62a935a4618cd05eb7d33be205a7
Eachin <EachinChung@gmail.com>
2022-03-25 12:30:39 +0800

    feat(clash): 快速破墙分组
    BREAKING CHANGE: 去除香港专线与中转分组，新增香港节点分组

--- script/refresh_clash_subscription.py
+++ script/refresh_clash_subscription.py
@@ -19,7 +19,7 @@ class Proxies(BaseModel):
     proxies: List[dict]
     proxy_names: List[str]
     proxy_names_of_hk_special_line: List[str]
-    proxy_names_of_hk_transit_line: List[str]
+    proxy_names_of_hk_node: List[str]
     proxy_names_of_us_node: List[str]
     proxy_names_of_jp_node: List[str]
     proxy_names_of_kr_node: List[str]
@@ -116,7 +116,7 @@ def node_name_matches_country(node_name: str) -> Tuple:  # noqa: C901
 async def get_clash_proxies() -> Proxies:
     proxy_names = []
     proxy_names_of_hk_special_line = []
-    proxy_names_of_hk_transit_line = []
+    proxy_names_of_hk_node = []
     proxy_names_of_us_node = []
     proxy_names_of_jp_node = []
     proxy_names_of_kr_node = []
@@ -135,8 +135,7 @@ async def get_clash_proxies() -> Proxies:
         if country == "HK":
             if "专线" in node_name or "腾讯内网" in node_name:
                 proxy_names_of_hk_special_line.append(node_name)
-            else:
-                proxy_names_of_hk_transit_line.append(node_name)
+            proxy_names_of_hk_node.append(node_name)
         elif country == "US":
             proxy_names_of_us_node.append(node_name)
         elif country == "JP":
@@ -150,7 +149,7 @@ async def get_clash_proxies() -> Proxies:
         proxies=proxies,
         proxy_names=proxy_names,
         proxy_names_of_hk_special_line=proxy_names_of_hk_special_line,
-        proxy_names_of_hk_transit_line=proxy_names_of_hk_transit_line,
+        proxy_names_of_hk_node=proxy_names_of_hk_node,
         proxy_names_of_us_node=proxy_names_of_us_node,
         proxy_names_of_jp_node=proxy_names_of_jp_node,
         proxy_names_of_kr_node=proxy_names_of_kr_node,
@@ -166,12 +165,12 @@ async def refresh_clash_subscription():
 
     clash["proxies"] = proxies.proxies
     clash["proxy-groups"][1]["proxies"].extend(proxies.proxy_names)
-    clash["proxy-groups"][2]["proxies"].extend(proxies.proxy_names)
+    clash["proxy-groups"][2]["proxies"].extend(proxies.proxy_names_of_hk_special_line)
     clash["proxy-groups"][3]["proxies"].extend(proxies.proxy_names)
     clash["proxy-groups"][4]["proxies"].extend(proxies.proxy_names)
+    clash["proxy-groups"][5]["proxies"].extend(proxies.proxy_names)
 
-    clash["proxy-groups"][24]["proxies"].extend(proxies.proxy_names_of_hk_special_line)
-    clash["proxy-groups"][25]["proxies"].extend(proxies.proxy_names_of_hk_transit_line)
+    clash["proxy-groups"][25]["proxies"].extend(proxies.proxy_names_of_hk_node)
     clash["proxy-groups"][26]["proxies"].extend(proxies.proxy_names_of_tw_node)
     clash["proxy-groups"][27]["proxies"].extend(proxies.proxy_names_of_us_node)
     clash["proxy-groups"][28]["proxies"].extend(proxies.proxy_names_of_jp_node)
