ab6ed4e563f0ab3546a6375857933999021cdd7b
Eachin <EachinChung@gmail.com>
2022-07-03 14:31:13 +0800

    feat: InterceptHandler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
@@ -8,6 +9,28 @@ from components.requests import close_requests, register_requests
 from script.checkin_daily import checkin_daily
 from script.refresh_clash_config import refresh_clash_config
 from script.refresh_clash_subscription import refresh_clash_subscription
+
+
+class InterceptHandler(logging.Handler):
+    """Intercepts builtin logging messages and routes them to Loguru"""
+
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+# Enable interceptor
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
 if __name__ == "__main__":
     logger.add("default.log", rotation="200KB", compression="zip", level="INFO")
